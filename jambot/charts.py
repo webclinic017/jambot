85e0e5bc1e0a736d8cf162b98706c32b338170e1
Jayme Gordon <jaymegordo@gmail.com>
2021-01-06 10:12:27 -0800

    Create better variable chart w broken out funcs

--- jambot/charts.py
+++ jambot/charts.py
@@ -1,13 +1,22 @@
-from datetime import (datetime as dt, timedelta as delta)
+import operator as opr
+from pickle import NONE
+import sys
+from datetime import datetime as dt
+from datetime import timedelta as delta
 
-import seaborn as sns
-from matplotlib import pyplot
 import plotly.graph_objs as go
 import plotly.offline as py
+import seaborn as sns
+from matplotlib import pyplot
 from plotly.subplots import make_subplots
 
-from . import (
-    functions as f)
+from . import functions as f
+
+colors = dict(
+    lightblue='#6df7ff',
+    lightred='#ff6d6d'
+)
+
 
 # Pandas DataFrame Styles
 def background_contracts(s):
@@ -98,8 +107,8 @@ def chart_orders(t, pre=36, post=None, width=900, fast=50, slow=200):
 
     fig = go.Figure()
     
-    fig.add_trace(go.Scatter(x=df['Timestamp'], y=df[f'ema{fast}'],line=dict(color='#18f27d', width=1)))
-    fig.add_trace(go.Scatter(x=df['Timestamp'], y=df[f'ema{slow}'],line=dict(color='#9d19fc', width=1)))
+    fig.add_trace(go.Scatter(x=df.Timestamp, y=df[f'ema{fast}'], line=dict(color='#18f27d', width=1)))
+    fig.add_trace(go.Scatter(x=df.Timestamp, y=df[f'ema{slow}'], line=dict(color='#9d19fc', width=1)))
     fig.add_trace(candlestick(df))
     fig.add_trace(labels)
     
@@ -123,8 +132,7 @@ def chart_orders(t, pre=36, post=None, width=900, fast=50, slow=200):
             overlaying='y',
             autorange=False,
             fixedrange=True,
-            range=[0,0.4])
-            )
+            range=[0,0.4]))
     fig.update_xaxes(
         autorange=True,
         tickformat=f.time_format(hrs=True),
@@ -135,87 +143,210 @@ def chart_orders(t, pre=36, post=None, width=900, fast=50, slow=200):
     
     fig.show()
 
-def candlestick(df):
-    trace = go.Candlestick(
-        x=df['Timestamp'],
-        open=df['Open'],
-        high=df['High'],
-        low=df['Low'],
-        close=df['Close'],
-        increasing=dict(line=dict(color='#6df7ff')),
-        decreasing=dict(line=dict(color='#ff6d6d')),
+def add_traces(df, fig, traces):
+    """Create traces from list of dicts and add to fig"""
+    def _append_trace(trace, m):
+        fig.append_trace(
+            trace=trace,
+            row=m.get('row', 1),
+            col=m.get('col', 1))
+
+    for m in traces:
+        m['df'] = m.get('df', df) # allow using different df (for balance)
+        traces = m['func'](**m)
+        if isinstance(traces, list):
+            for trace in traces:
+                _append_trace(trace, m)
+        else:
+            _append_trace(traces, m)
+
+def candlestick(df, **kw):
+    return go.Candlestick(
+        name='candles',
+        x=df.index,
+        open=df.Open,
+        high=df.High,
+        low=df.Low,
+        close=df.Close,
+        increasing=dict(line=dict(color=colors['lightblue'])),
+        decreasing=dict(line=dict(color=colors['lightred'])),
         line=dict(width=1),
         hoverlabel=dict(split=False, bgcolor='rgba(0,0,0,0)'))
-    return trace
 
-def chart(df, symbol, df2=None):
-    py.init_notebook_mode(connected=False)
+def scatter(df, name, color, hoverinfo='all', **kw):
+    return go.Scatter(
+        name=name,
+        x=df.index,
+        y=df[name],
+        mode='lines',
+        line=dict(color=color, width=1),
+        hoverinfo=hoverinfo)
 
-    fig = make_subplots(rows=3, cols=1, shared_xaxes=True, row_width=[0.1, 0.1, 0.4])
-    fig.append_trace(candlestick(df), row=1, col=1)
+def predictions(df, name, **kw):
+    """Add traces of predicted 1, 0, -1 vals as shapes"""
+    df = df.copy()
+    # TODO 200 needs to be a %, not hard coded
+    df['sma_low'] = df.Low.rolling(10).mean() - 200
+    df['sma_low'] = df[['sma_low', 'Low']].min(axis=1) - 200
+    df['sma_low'] = df['sma_low'].ewm(span=5).mean()
+    df['sma_low'] = df[['sma_low', 'Low']].min(axis=1) - 200
+
+    m = {
+        'triangle-up': [1, 'green'],
+        'circle': [0, '#8c8c8c'],
+        'triangle-down': [-1, 'red']}
+        
+    traces = []
+
+    for name, v in m.items():
+        df2 = df[df.y_pred == v[0]]
+
+        # filter to prediction correct/incorrect
+        m3 = {'': opr.eq, '-open': opr.ne}
+        for shape_suff, op in m3.items():
+            df3 = df2[op(df2.y_pred, df2.target)]
+
+            # set hover label to proba % text
+            fmt = lambda x, y: f'{y}: {x:.1%}'
+            text = df3.index.strftime('%Y-%m-%d %H') + '<br>' + \
+                df3.proba_long.apply(fmt, y='long') + '<br>' + \
+                df3.proba_short.apply(fmt, y='short')
+
+            trace = go.Scatter(
+                name='preds',
+                x=df3.index,
+                y=df3.sma_low,
+                mode='markers',
+                marker=dict(
+                    size=4,
+                    color=v[1],
+                    symbol=f'{name}{shape_suff}'),
+                text=text,
+                hoverinfo='text')
+
+            traces.append(trace)
     
-    # add ema cols 
-    for col in df.columns:
-        if 'ema' in col:
-            if '50' in col: sColour = '#18f27d'
-            if '200' in col: sColour = '#9d19fc'
-            emaTrace = go.Scatter(
-                x=df['Timestamp'],
-                y=df[col],
-                mode='lines',
-                line=dict(color=sColour, width=1))
-            fig.append_trace(emaTrace, row=1, col=1)
+    return traces
 
-    df3 = df2.loc[df2['PercentChange'] < 0]
-    percentTrace = go.Bar(x=df3['Timestamp'], 
-               y=df3['PercentChange'], 
-               marker=dict(line=dict(width=2, color='#ff7a7a')))
-    fig.append_trace(percentTrace, row=2, col=1)
+def probas(df, **kw):
+    df = df.copy()
+    df.proba_short = df.proba_short * -1
+    traces = []
+
+    names = dict(
+        long=colors['lightblue'],
+        short=colors['lightred']
+        )
+
+    for name, color in names.items():
+        trace = go.Bar(
+            name=name,
+            x=df.index,
+            y=df[f'proba_{name}'],
+            marker=dict(
+                line=dict(width=0),
+                color=color),
+            offsetgroup=0
+        )
+        
+        traces.append(trace)
+
+    return traces
+
+def chart(df, symbol='XBTUSD', periods=200, last=True, startdate=None, df_balance=None):
+
+    # py.init_notebook_mode(connected=False)
+
+    traces = [
+        dict(name='ema10', func=scatter, color='orange', hoverinfo='skip'),
+        dict(name='ema50', func=scatter, color='#18f27d', hoverinfo='skip'),
+        dict(name='ema200', func=scatter, color='#9d19fc', hoverinfo='skip'),
+        dict(name='y_pred', func=predictions),
+        dict(name='candle', func=candlestick),
+        dict(name='probas', func=probas, row=2),
+        # dict(name='vty_ema', func=scatter, color='green', row=2),
+        # dict(name='vty_sma', func=scatter, color='yellow', row=2),
+        # dict(name='rsi', func=scatter, color='purple', row=2),
+        # dict(name='rsi_stoch', func=scatter, color='red', row=2),
+        # dict(name='rsi_stoch_k', func=scatter, color='blue', row=2),
+        # dict(name='rsi_stoch_d', func=scatter, color='green', row=2),
+        ]
     
-    df3 = df2.loc[df2['PercentChange'] >= 0]
-    percentTrace = go.Bar(x=df3['Timestamp'], 
-               y=df3['PercentChange'], 
-               marker=dict(line=dict(width=2, color='#91ffff')))
-    fig.append_trace(percentTrace, row=2, col=1)
+    if not df_balance is None:
+        traces.append(dict(name='balance', func=scatter, color='#91ffff', row=3))
+        df = df.merge(right=df_balance, how='left', left_index=True, right_index=True) \
+            .assign(balance=lambda x: x.balance.fillna(method='ffill'))
+        # display(df.tail(20))
 
-    balanceTrace = go.Scatter(
-                    x=df2['Timestamp'],
-                    y=df2['Balance'],
-                    mode='lines',
-                    line=dict(color='#91ffff', width=1))
-    fig.append_trace(balanceTrace, row=3, col=1)
+    if startdate:
+        df = df[df.index >= startdate] \
+            .iloc[:periods, :]
+    elif last:
+        df = df.iloc[-1 * periods:, :]
+    else:
+        df = df.iloc[:periods, :]
+    
+    rows = max([m.get('row', 1) for m in traces])
+    row_widths = [0.2, 0.2, 0.6][rows * -1:]
+    height = 1000 * sum(row_widths)
+    fig = make_subplots(rows=rows, cols=1, shared_xaxes=True, row_width=row_widths)
+    fig = go.FigureWidget(fig)
+
+    def _zoom(layout, xrange, *args, **kw):
+        print('changing')
+        in_view = df.loc[fig.layout.xaxis.range[0]: fig.layout.xaxis.range[1]]
+        fig.layout.yaxis.range = [in_view.Low.min() - 10, in_view.High.max() + 10]
+    
+    fig.layout.on_change(_zoom, 'xaxis.range')
+
+    add_traces(df, fig, traces)
+
+    xaxis = dict(
+        tickformat=f.time_format(hrs=True),
+        rangeslider_thickness=0.05,
+        tickangle=315)
+
+    # update all axes in subplots
+    fig.update_xaxes(
+        gridcolor='#182633',
+        autorange=True,
+        fixedrange=False)
+    fig.update_yaxes(
+        gridcolor='#182633',
+        side='right',
+        autorange=True,
+        fixedrange=False)
 
     fig.update_layout(
-        height=800,
+        xaxis=xaxis,
+        height=height,
+        # hovermode='x unified',
+        # width=1000,
+        margin=dict(l=0, r=0, t=40, b=0),
         paper_bgcolor='#000B15',
         plot_bgcolor='#000B15',
         font_color='white',
         showlegend=False,
-        title=symbol + ' OHLC')
-    fig.update_xaxes(
-        autorange=True,
-        tickformat=f.time_format(hrs=True),
-        gridcolor='#e6e6e6',
-        showgrid=True,
-        gridwidth=1,
-        tickangle=315)
-    fig.update_yaxes(
-        autorange=True,
-        fixedrange=False,
-        side='right',
-        row=1, col=1)
-    fig.update_yaxes(
-        showgrid=False,
-        autorange=True,
-        fixedrange=False,
-        side='right',
-        row=2, col=1)
-    fig.update_yaxes(
-        showgrid=False,
-        autorange=True,
-        fixedrange=False,
-        side='right',
-        row=3, col=1)
+        title=symbol)
 
     return fig
  
+    # if not df2 is None:
+    #     df3 = df2.loc[df2['PercentChange'] < 0]
+    #     percentTrace = go.Bar(x=df3['Timestamp'], 
+    #             y=df3['PercentChange'], 
+    #             marker=dict(line=dict(width=2, color='#ff7a7a')))
+    #     fig.append_trace(percentTrace, row=2, col=1)
+        
+    #     df3 = df2.loc[df2['PercentChange'] >= 0]
+    #     percentTrace = go.Bar(x=df3['Timestamp'], 
+    #             y=df3['PercentChange'], 
+    #             marker=dict(line=dict(width=2, color='#91ffff')))
+    #     fig.append_trace(percentTrace, row=2, col=1)
+
+    #     balanceTrace = go.Scatter(
+    #                     x=df2['Timestamp'],
+    #                     y=df2['Balance'],
+    #                     mode='lines',
+    #                     line=dict(color='#91ffff', width=1))
+    #     fig.append_trace(balanceTrace, row=3, col=1)
