dece2f51a289e2f66388cb34284856755f9d7475
Jayme Gordon <jaymegordo@gmail.com>
2021-05-23 11:36:17 -0700

    Add extra properties and df_trades

--- jambot/tradesys/strategies/base.py
+++ jambot/tradesys/strategies/base.py
@@ -7,20 +7,11 @@ from .__init__ import *
 class StrategyBase(Observer):
     def __init__(self, symbol: str, weight=1, lev=5, slippage=0.02, **kw):
         super().__init__(**kw)
-        i = 1
-        # status = 0
-        # self.weight = weight
-        # entry_price = 0
-        # exit_price = 0
-        # maxspread = 0.1
-        # self.slippage = slippage
-        # self.lev = lev
-        # unfilledtrades = 0
-        # timeout = float('inf')
-        trades = []
 
+        trades = []
         broker = Broker(parent_listener=self)
         wallet = broker.get_wallet(symbol)
+        wallet.lev = lev  # NOTE would prefer to init with this
 
         f.set_self(vars())
 
@@ -35,12 +26,23 @@ class StrategyBase(Observer):
 
     @property
     def num_trades(self):
+        """Total number of trades"""
         return len(self.trades)
+
+    @property
+    def num_trades_filled(self):
+        """Total number of trades where at least one order was executed"""
+        return len([t for t in self.trades if abs(t.qty) > 0])
 
     @property
     def trade(self) -> 'Trade':
         """Return last (current) trade"""
         return self.trades[-1] if len(self.trades) > 0 else None
+
+    @property
+    def tpd(self):
+        """Trades per day frequency"""
+        return self.num_trades / (self.duration / 24)
 
     def add_trade(self, trade: 'Trade'):
         self.trades.append(trade)
@@ -57,21 +59,9 @@ class StrategyBase(Observer):
 
         return trade
 
-    def last_trade(self):
-        return self.trades[self.tradecount() - 1]
-
-    def get_trade(self, i):
-        numtrades = self.tradecount()
-        if i > numtrades:
-            i = numtrades
-        return self.trades[i - 1]
-
-    def good_trades(self):
-        count = 0
-        for t in self.trades:
-            if t.is_good():
-                count += 1
-        return count
+    @property
+    def good_trades(self) -> int:
+        return sum([t.is_good for t in self.trades])
 
     def get_side(self):
         status = self.status
@@ -85,51 +75,40 @@ class StrategyBase(Observer):
     def bad_trades(self):
         return list(filter(lambda x: x.pnlfinal <= 0, self.trades))
 
-    def print_trades(self, maxmin=0, first=float('inf'), last=0, df=None):
+    def show_trades(self, maxmin=0, first=float('inf'), last=0, df=None):
         import seaborn as sns
 
         if df is None:
             df = self.df_trades(first=first, last=last)
         style = df.style.hide_index()
 
-        figs = self.bm.decimal_figs
-        price_format = '{:,.' + str(figs) + 'f}'
+        # figs = self.bm.decimal_figs
+        # price_format = '{:,.' + str(figs) + 'f}'
+        price_format = '{:,.0f}'
 
         cmap = sns.diverging_palette(10, 240, sep=10, n=20, center='dark', as_cmap=True)
-        style.background_gradient(cmap=cmap, subset=['Pnl'], vmin=-0.1, vmax=0.1)
-        style.background_gradient(cmap=cmap, subset=['PnlAcct'], vmin=-0.3, vmax=0.3)
+        # style.background_gradient(cmap=cmap, subset=['PnlAcct'], vmin=-0.3, vmax=0.3)
 
-        style.format({'Timestamp': '{:%Y-%m-%d %H}',
-                      'Contracts': '{:,}',
-                      'Entry': price_format,
-                      'Exit': price_format,
-                      'Conf': '{:.3f}',
-                      'Pnl': '{:.2%}',
-                      'PnlAcct': '{:.2%}',
-                      'Bal': '{:.2f}'})
+        style \
+            .background_gradient(cmap=cmap, subset=['pnl'], vmin=-0.1, vmax=0.1) \
+            .format({
+                'timestamp': '{:%Y-%m-%d %H}',
+                'qty': '{:,}',
+                'entry': price_format,
+                'exit': price_format,
+                #   'Conf': '{:.3f}',
+                'pnl': '{:.2%}',
+                #   'PnlAcct': '{:.2%}',
+                'bal': '{:.2f}'
+            })
+
         display(style)
 
     def df_trades(self, first=float('inf'), last=0):
         """Return df of trade history"""
-        data = []
         trades = self.trades
-        cols = ['N', 'Timestamp', 'Sts', 'Dur', 'Entry', 'Exit',
-                'Contracts', 'Conf', 'Pnl', 'PnlAcct', 'Bal']  # 'Market',
+        data = [t.dict_stats() for t in trades[last * -1: min(first, len(trades))]]
 
-        for t in trades[last * -1: min(first, len(trades))]:
-            data.append([
-                t.tradenum,
-                t.candles[0].Index,
-                t.status,
-                t.duration(),
-                t.entry_price,
-                t.exit_price,
-                # t.exit_order().ordtype_str(),
-                t.filledcontracts,
-                t.conf,
-                t.pnlfinal,
-                t.pnl_acct(),
-                t.exitbalance])
-
-        return pd.DataFrame.from_records(data=data, columns=cols) \
-            .assign(profitable=lambda x: x.Pnl > 0)
+        return pd.DataFrame \
+            .from_dict(data=data) \
+            .assign(profitable=lambda x: x.pnl > 0)
