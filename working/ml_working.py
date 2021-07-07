708536be61ba3230903232f732784557b355632b
Jayme Gordon <jaymegordo@gmail.com>
2021-07-07 13:01:36 -0700

    Testing

--- working/ml_working.py
+++ working/ml_working.py
@@ -25,23 +25,21 @@ if True:
     from lightgbm.sklearn import LGBMClassifier, LGBMRegressor
     from sklearn.compose import ColumnTransformer, make_column_transformer
     from sklearn.decomposition import PCA
-    # Classifiers
     from sklearn.dummy import DummyClassifier
     from sklearn.ensemble import (AdaBoostClassifier,
                                   GradientBoostingClassifier,
                                   RandomForestClassifier)
-    # other
     from sklearn.feature_selection import RFE, RFECV
     from sklearn.impute import SimpleImputer
-    # classifiers / models
     from sklearn.linear_model import (LinearRegression, LogisticRegression,
-                                      Ridge, RidgeCV)
+                                      Ridge, RidgeClassifier, RidgeCV)
     from sklearn.metrics import (accuracy_score, classification_report,
                                  f1_score, make_scorer, mean_squared_error,
                                  recall_score)
     from sklearn.model_selection import (GridSearchCV, RandomizedSearchCV,
                                          TimeSeriesSplit, cross_val_score,
                                          cross_validate, train_test_split)
+    from sklearn.multioutput import MultiOutputRegressor
     from sklearn.pipeline import FeatureUnion, Pipeline, make_pipeline
     from sklearn.preprocessing import (MinMaxScaler, OneHotEncoder,
                                        OrdinalEncoder, PolynomialFeatures,
@@ -51,19 +49,16 @@ if True:
                               export_graphviz)
     from xgboost import XGBClassifier
 
-    # from jambot import livetrading as live
-    # from jambot import optimization as op
     from jambot import charts as ch
     from jambot import data
     from jambot import functions as f
     from jambot import signals as sg
-    from jambot import sklearn_helper_funcs as sf
+    from jambot import sklearn_utils as sk
     from jambot.database import db
     from jambot.tradesys import backtest as bt
     from jambot.tradesys.strategies import ml
 
-    # from jambot.strategies import trendrev
-    # from mlxtend.feature_selection import SequentialFeatureSelector
+    plt.rcParams.update({'figure.figsize': (12, 5)})
 
 # %% - LOAD DF
 if True:
@@ -103,8 +98,7 @@ regression = False
 # pct_min = 0.000000005 # long or short, no neutral
 pct_min = 0
 target_signal = Target(p_ema=p_ema, n_periods=n_periods, pct_min=pct_min, regression=regression)
-# sm_target = sg.SignalManager()
-# df = df.pipe(sm_target.add_signals, signals=[target_signal])
+# target_signal = sg.TargetMaxMin(n_periods=4)
 
 signals = [
     'EMA',
@@ -123,33 +117,36 @@ signals = [
 # drop last rows which we cant set proper target
 df = sm.add_signals(df=df, signals=signals) \
     .iloc[:-1 * n_periods, :] \
-    .dropna()
+    .fillna(0)
 
 if not regression:
-    sf.show_prop(df=df)
+    sk.show_prop(df=df)
 
 # %% - FEATURES, MODELMANAGER
 if True:
     cols_ohlcv = ['Open', 'High', 'Low', 'Close', 'VolBTC']
+    # , 'target_max', 'target_min', 'pred_max', 'pred_min']
     drop_feats = cols_ohlcv + ['ema10', 'ema50', 'ema200', 'pxhigh', 'pxlow']
+    # target = ['target_max', 'target_min']
+    target = ['target']
 
     features = dict(
-        # signalmanager=cols_ohlcv,
-        target=['target'],
+        target=target,
         drop=drop_feats,
-        passthrough=['ema_trend', 'ema_slope', 'cdl_side', 'macd_trend']
+        # passthrough=['ema_trend', 'ema_slope', 'cdl_side', 'macd_trend']
     )
 
-    features['passthrough'] = features['passthrough'] + \
-        [col for col in df.columns if any(item in col for item in ('sfp', 'prevhigh', 'prevlow'))]
-    features['numeric'] = sf.all_except(df, features.values())
+    # features['passthrough'] = features['passthrough'] + \
+    #     [col for col in df.columns if any(item in col for item in ('sfp', 'prevhigh', 'prevlow'))]
+
+    features['numeric'] = sk.all_except(df, features.values())
 
     # remove any cols not in df
     features = {name: [col for col in cols if col in df.columns] for name, cols in features.items()}
 
     encoders = dict(
         drop='drop',
-        passthrough='passthrough',
+        # passthrough='passthrough',
         numeric=MinMaxScaler(feature_range=(0, 1)))
 
     n_splits = 5
@@ -166,8 +163,7 @@ if True:
         scoring = dict(
             acc='accuracy',
             max=max_scorer,
-            final=final_scorer
-        )
+            final=final_scorer)
     else:
         scoring = dict(rmse='neg_root_mean_squared_error')
 
@@ -176,15 +172,17 @@ if True:
     #     lgbm__sample_weight=lambda x: np.linspace(0.5, 1, x.shape[0]))
     fit_params = None
 
-    cv_args = dict(cv=cv, n_jobs=1, return_train_score=True, scoring=scoring, fit_params=fit_params)
-    mm = sf.ModelManager(scoring=scoring, cv_args=cv_args)
+    cv_args = dict(cv=cv, n_jobs=1, return_train_score=True, scoring=scoring)
+    mm = sk.ModelManager(scoring=scoring, cv_args=cv_args)
     ct = mm.make_column_transformer(features=features, encoders=encoders)
 
-    x_train, y_train, x_test, y_test = mm.make_train_test(
+    x_train, y_train, x_test, _ = mm.make_train_test(
         df=df,
-        target=features['target'],
-        train_size=train_size,
-        shuffle=False)
+        target=target,
+        split_date=dt(2021, 1, 1),
+        # train_size=train_size,
+        # shuffle=False
+    )
 
 # %% - MODELS
 LGBM = LGBMRegressor if regression else LGBMClassifier
@@ -195,15 +193,32 @@ models = dict(
     # ada=AdaBoostClassifier(),
     # lgbm=LGBM,
     lgbm=LGBM(num_leaves=50, n_estimators=50, max_depth=10, boosting_type='dart')
+    # lgbm=LGBM(num_leaves=50, n_estimators=50, max_depth=30, boosting_type='gbdt')
 )
 
 steps = [
-    (1, ('pca', PCA(n_components=20, random_state=mm.random_state)))]
+    (1, ('pca', PCA(n_components=20, random_state=0)))]
 # steps = None
 
 scorer.reset()
 mm.cross_val(models, steps=steps)
 scorer.show_summary()
+
+# %% - MAXMIN PREDS
+
+if False:
+    models_multi = dict(
+        lgbm=LGBMRegressor(num_leaves=50, n_estimators=50, max_depth=10, boosting_type='dart'),
+        ridge=Ridge()
+    )
+
+    models_multi = sk.as_multi_out(models_multi)
+
+    steps = [
+        (1, ('pca', PCA(n_components=20, random_state=0)))]
+    # steps = None
+
+    mm.cross_val(models_multi, steps=steps)
 
 # %% - OPTIMIZE FEATURES
 if False:
@@ -214,27 +229,35 @@ if False:
     windows = [96]
     params = dict(trend_trix=dict(window=windows))  # , window_slow=windows, cycle=windows))
     # params = dict(trend_cci=dict(constant=[0.05, 0.01, 0.015, 0.02, 0.04, 0.05, 0.06]))
-    sf.pretty_dict(params)
+    sk.pretty_dict(params)
 
     name = 'lgbm'
     mm.cross_val_feature_params(signal_manager=sm, name=name, model=models[name], feature_params=params)
 
 # %% - GRID - LGBMClassifier
 
-best_est = False
+best_est = True
 if best_est:
     params = dict(
-        boosting_type=['gbdt', 'dart', 'goss', 'rf'],
+        boosting_type=['gbdt', 'dart'],  # 'goss' , 'rf'
         n_estimators=[25, 50, 100],
         max_depth=[-1, 5, 10, 20, 30],
         num_leaves=[5, 10, 20, 40, 100],
     )
 
+    # ridge
+    # params = dict(
+    #     alpha=[0.1, 0.5, 1, 5, 10, 100]
+    # )
+
     grid = mm.search(
         name='lgbm',
         params=params,
-        search_type='grid',
-        refit='acc')
+        # search_type='grid',
+        # refit='acc',
+        # refit='rmse',
+        refit='final'
+    )
 
 # %% - CLASSIFICATION REPORT
 # mm.class_rep('lgbm', best_est=best_est)
@@ -253,36 +276,10 @@ if run_ada:
         search_type='random',
         refit='acc')
 
-# %% - ADD PREDICT
-name = 'lgbm'
-model = models[name]
-# df2 = df[df.index > '2020-06-01'].copy()
-
-n_smooth = 6
-rolling_col = 'proba_long' if not regression else 'y_pred'
-
-if True:
-    df_pred = mm \
-        .add_predict_iter(
-            df=df,
-            name=name,
-            pipe=mm.pipes[name],
-            batch_size=24 * 2,
-            max_train_size=None,
-            regression=regression) \
-        .pipe(sg.add_ema, p=n_smooth, c=rolling_col, col='rolling_proba') \
-        .assign(
-            signal=lambda x: np.sign(np.diff(np.sign(x.rolling_proba - 0.5), prepend=np.array([0])))) \
-        .fillna(0)
-    # df_pred = df_pred \
-    #     .assign(rolling_proba=lambda x: x[rolling_col].rolling(n_smooth).mean())
-    df_pred_iter = df_pred.copy()
-    # df_pred = df_pred_iter.copy()
-
 # %% - RUN STRAT
 
-fit_params = dict(
-    lgbm__sample_weight=np.linspace(0.5, 1, mm.df_train.shape[0]))
+name = 'lgbm'
+# fit_params = sk.weighted_fit(name, n=mm.df_train.shape[0])
 fit_params = None
 
 # import talib as tb
@@ -290,23 +287,46 @@ fit_params = None
 # df['y_pred'] = np.where(df.Close > df.psar, -1, 1)
 # df_pred = df.copy()
 
-# df_pred = mm \
-#     .add_predict(
-#         df=df,
-#         name=name,
-#         best_est=False,
-#         proba=not regression,
-#         fit_params=fit_params) \
-#     .pipe(sg.add_ema, p=n_smooth, c=rolling_col, col='rolling_proba') \
-#     .assign(
-#         signal=lambda x: np.sign(np.diff(np.sign(x.rolling_proba - 0.5), prepend=np.array([0])))) \
-#     .fillna(0)
-# .assign(rolling_proba=lambda x: x[rolling_col].rolling(n_smooth).mean())
+# TODO test iter_predict maxhigh/minlow
+
+if True:
+    # retrain every x hours (24 ish) and predict for the test set
+    df_pred = mm \
+        .add_predict_iter(
+            df=df,
+            name=name,
+            pipe=mm.pipes[name],
+            batch_size=24 * 2,
+            min_size=mm.df_train.shape[0],
+            max_train_size=None,
+            regression=regression)
+
+    df_pred_iter = df_pred.copy()
+    # df_pred = df_pred_iter.copy()
+
+else:
+    df_pred = mm \
+        .add_predict(
+            df=df,
+            name=name,
+            proba=not regression,
+            fit_params=fit_params)
+
+n_smooth = 6
+rolling_col = 'proba_long' if not regression else 'y_pred'
+
+df_pred = df_pred \
+    .pipe(sg.add_ema, p=n_smooth, c=rolling_col, col='rolling_proba') \
+    .assign(signal=lambda x: sk.proba_to_signal(x.rolling_proba)) \
+    # .pipe(sk.add_preds_minmax, features, encoders, train_size)
+
+# df_pred[['pred_max', 'pred_min']] = df_pred[['pred_max', 'pred_min']].rolling(12).mean()
 
 strat = ml.Strategy(
     lev=3,
     slippage=0,
     regression=regression,
+    # market_on_timeout=True,
     # stoppercent=-0.025,
 )
 
@@ -319,8 +339,9 @@ kw = dict(
     # interval=1
 )
 
-cols = ['Open', 'High', 'Low', 'Close', 'y_pred', 'proba_long', 'rolling_proba', 'signal']
-bm = bt.BacktestManager(**kw, strat=strat, df=df_pred[cols])
+cols = ['Open', 'High', 'Low', 'Close', 'y_pred', 'proba_long', 'rolling_proba',
+        'signal', 'pred_max', 'pred_min', 'target_max', 'target_min']
+bm = bt.BacktestManager(**kw, strat=strat, df=df_pred.pipe(f.clean_cols, cols))
 bm.run()
 bm.print_final()
 wallet = strat.wallet
@@ -332,8 +353,9 @@ t = trades[-1]
 
 periods = 30 * 24
 # periods = 400
-startdate = dt(2021, 1, 4)
-startdate = None
+startdate = dt(2021, 4, 15)
+startdate = dt(2021, 1, 1)
+# startdate = None
 split_val = 0.5 if not regression else 0
 
 df_chart = df_pred[df_pred.index >= x_test.index[0]]
@@ -376,8 +398,7 @@ if not df_trades is None:
         .set_index('timestamp') \
         .rename(columns=rename_cols)  # [rename_cols.values()]
 
-    df_chart = df_chart \
-        .merge(right=df_trades, how='left', left_index=True, right_index=True)
+    df_chart = df_chart.pipe(f.left_merge, df_trades)
 
 # enumerate row numbers
 # traces = [{**m, **dict(row=m.get('row', i+2))} for i, m in enumerate(traces)]
@@ -405,7 +426,7 @@ mm.cross_val(models, steps=extra_steps)
 
 # %% - RFECV SHOW FEATURES
 data = ct.fit_transform(x_train)
-df_trans = sf.df_transformed(data=data, ct=ct).describe().T
+df_trans = sk.df_transformed(data=data, ct=ct).describe().T
 
 pipe_rfe = mm.pipes['lgbm_rfecv']
 pipe_rfe.fit(x_train, y_train)  # need fit pipe again outside of cross_validate
@@ -414,7 +435,7 @@ rfecv.n_features_
 
 pd.DataFrame(data=rfecv.support_, columns=['Included'], index=df_trans.index) \
     .style \
-    .apply(sf.highlight_val, subset=['Included'], m={True: (ch.colors['lightblue'], 'black')})
+    .apply(sk.highlight_val, subset=['Included'], m={True: (ch.colors['lightblue'], 'black')})
 
 # %%
 # POLY
