1b633f556ea6ad37de49feb73e9ac2c4398e44f6
Jayme Gordon <jaymegordo@gmail.com>
2021-08-17 13:39:51 -0700

    Create FifteenMin

--- az_FifteenMin/__init__.py
+++ az_FifteenMin/__init__.py
@@ -1,11 +1,10 @@
-
-from datetime import datetime as dt
-from datetime import timedelta as delta
+from datetime import datetime as dt  # noqa
+from datetime import timedelta as delta  # noqa
 
 import azure.functions as func
 from __app__.jambot import SYMBOL  # type: ignore
 from __app__.jambot import functions as f  # type: ignore
-from __app__.jambot import livetrading as live  # type: ignore
+from __app__.jambot import livetrading as live  # type: ignore # noqa
 from __app__.jambot.database import db  # type: ignore
 from __app__.jambot.exchanges.bitmex import Bitmex  # type: ignore
 
@@ -15,10 +14,15 @@ def main(mytimer: func.TimerRequest) -> None:
         exch = Bitmex.default(test=False)
         db.update_all_symbols(exch=exch, interval=15, symbol=SYMBOL)
 
-        startdate, daterange = dt.now().date() + delta(days=-15), 30
-        dfall = db.get_dataframe(symbol='XBTUSD', startdate=startdate, daterange=daterange)
+        # startdate, daterange = dt.now().date() + delta(days=-15), 30
+        # dfall = db.get_df(symbol='XBTUSD', startdate=startdate, daterange=daterange)
 
         # live.run_toploop(u=u, dfall=dfall)
         # live.check_sfp(df=dfall)
+
+        # update hourly candles after strat run on 15 min
+        if 0 <= dt.utcnow().minute < 15:
+            db.update_all_symbols(exch=exch, interval=1, symbol=SYMBOL)
+
     except:
         f.send_error()
