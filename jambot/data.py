3d91349f3fe0d7e9a156d2e97999fd3e1d41e79a
Jayme Gordon <jaymegordo@gmail.com>
2021-08-30 10:32:23 -0700

    Convert dtypes on read_csv

--- jambot/data.py
+++ jambot/data.py
@@ -4,6 +4,7 @@ Loading dataframe
 
 from pathlib import Path
 
+import numpy as np
 import pandas as pd
 
 
@@ -11,8 +12,14 @@ def default_df():
     """Get simple default df for testing"""
     p = Path('df.csv')
 
+    # NOTE not dry - defined in database.get_db()
+    cols = ['timestamp', 'open', 'high', 'low', 'close', 'volume']
+    dtypes = {c: np.float32 for c in cols[1:]}
+    dtypes['volume'] = pd.Int64Dtype()
+
     return pd \
         .read_csv(
             p,
             parse_dates=['timestamp'],
-            index_col='timestamp')
+            index_col='timestamp') \
+        .astype(dtypes)
