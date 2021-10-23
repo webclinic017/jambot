86cc39cf71c8f653b8a5ac77bf4dace9da4437fb
Jayme Gordon <jaymegordo@gmail.com>
2021-10-22 17:29:02 -0700

    Refresh bybit data

--- az_FifteenMin/__init__.py
+++ az_FifteenMin/__init__.py
@@ -2,24 +2,28 @@ from datetime import datetime as dt  # noqa
 from datetime import timedelta as delta  # noqa
 
 import azure.functions as func
-from __app__.jambot import SYMBOL  # type: ignore
 from __app__.jambot import functions as f  # type: ignore
 from __app__.jambot import livetrading as live  # type: ignore # noqa
 from __app__.jambot.database import db  # type: ignore
 from __app__.jambot.exchanges.bitmex import Bitmex  # type: ignore
+from __app__.jambot.exchanges.bybit import Bybit  # type: ignore
 
 
 def main(mytimer: func.TimerRequest) -> None:
     try:
+        # update 15 min candles for both bitmex + bybit
         interval = 15
-        exch = Bitmex.default(test=False, refresh=False)
-        db.update_all_symbols(exch=exch, interval=interval, symbol=SYMBOL)
+        bmex = Bitmex.default(test=False, refresh=False)
+        bbit = Bybit.default(test=False, refresh=False)
+        exchs = [bmex, bbit]
+
+        db.update_all_symbols(exchs=exchs, interval=interval)
 
         live.run_strat_live(interval=interval)
 
-        # update hourly candles after strat run on 15 min
+        # update hourly candles after strat run on 15 min (bitmex only for now)
         if 0 <= dt.utcnow().minute < 15:
-            db.update_all_symbols(exch=exch, interval=1, symbol=SYMBOL)
+            db.update_all_symbols(exchs=bmex, interval=1)
 
     except:
         f.send_error()
