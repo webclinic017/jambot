0e87ab3498978beb2b5d777c02bae0d2cfb66c0b
Jayme Gordon <jaymegordo@gmail.com>
2021-11-03 18:15:30 -0700

    Add funding_rate

--- jambot/data.py
+++ jambot/data.py
@@ -13,13 +13,16 @@ def default_df():
     p = Path('df.csv')
 
     # NOTE not dry - defined in database.get_db()
-    cols = ['timestamp', 'open', 'high', 'low', 'close', 'volume']
+    cols = ['timestamp', 'open', 'high', 'low', 'close', 'volume', 'funding_rate']
     dtypes = {c: np.float32 for c in cols[1:]}
     dtypes['volume'] = pd.Int64Dtype()
+    dtypes['funding_rate'] = np.float32
 
-    return pd \
+    df = pd \
         .read_csv(
             p,
             parse_dates=['timestamp'],
-            index_col='timestamp') \
-        .astype(dtypes)
+            index_col='timestamp')
+
+    dtypes = {k: v for k, v in dtypes.items() if k in df.columns}
+    return df.astype(dtypes)
