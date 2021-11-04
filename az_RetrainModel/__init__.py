f54b296baf5d61a9de14e6202f0e9cf66bb7da72
Jayme Gordon <jaymegordo@gmail.com>
2021-11-04 15:31:23 -0700

    Add ExchangeManager

--- az_RetrainModel/__init__.py
+++ az_RetrainModel/__init__.py
@@ -1,15 +1,19 @@
+# type: ignore
+
 try:
     import azure.functions as func
-    from __app__.jambot import comm as cm  # type: ignore
-    from __app__.jambot.ml import storage as st  # type: ignore
+    from __app__.jambot import comm as cm
+    from __app__.jambot.livetrading import ExchangeManager
+    from __app__.jambot.ml import storage as st
 except:
-    from __app__.jambot import comm as cm  # type: ignore
+    from __app__.jambot import comm as cm
     cm.send_error()
 
 
 # NOTE might need to set this to run on deploy, packages probably not saved on new deploy
 def main(mytimer: func.TimerRequest) -> None:
     try:
-        st.ModelStorageManager().fit_save_models()
+        em = ExchangeManager()
+        st.ModelStorageManager().fit_save_models(em=em)
     except:
         cm.send_error()
