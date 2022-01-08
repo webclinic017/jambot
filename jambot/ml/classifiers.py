c361ada00c8f94e5604f52e608b3bb3851bce701
Jayme Gordon <jaymegordo@gmail.com>
2022-01-07 19:15:32 -0800

    Create loggable LGBMClassifier

---
+++ jambot/ml/classifiers.py
@@ -0,0 +1,20 @@
+from typing import *
+
+from lightgbm import LGBMClassifier
+
+from jambot.utils.mlflow import MlflowLoggable
+
+
+class LGBMClsLog(LGBMClassifier, MlflowLoggable):
+    """Wrapper to make LGBMClassifier easily loggable with mlflow"""
+
+    log_keys = [
+        'boosting_type',
+        'num_leaves',
+        'max_depth',
+        'learning_rate',
+        'n_estimators']
+
+    @property
+    def log_items(self) -> Dict[str, Any]:
+        return {k: v for k, v in self.__dict__.items() if k in self.log_keys}
