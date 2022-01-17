0503e92c2df39e20be36a39a42a1c0015d60d083
Jayme Gordon <jaymegordo@gmail.com>
2022-01-17 08:13:00 -0800

    Expand space, add caching

--- jambot/utils/skopt.py
+++ jambot/utils/skopt.py
@@ -1,3 +1,5 @@
+import time
+from collections import defaultdict
 from typing import *
 
 import mlflow
@@ -9,7 +11,7 @@ from skopt.callbacks import CheckpointSaver
 from skopt.space import Integer, Real
 
 from jambot import config as cf
-from jambot import data, getlog
+from jambot import getlog
 from jambot import sklearn_utils as sk
 from jambot.ml import models as md
 from jambot.ml.classifiers import LGBMClsLog
@@ -28,7 +30,8 @@ if TYPE_CHECKING:
 
 log = getlog(__name__)
 
-ACTIVE_RESULT = 8
+ACTIVE_RESULT = 14
+MLFLOW_EXP = str(9)  # TODO make this set auto with new run
 p_skopt = cf.p_data / 'skopt'
 p_res = p_skopt / f'results{ACTIVE_RESULT}.pkl'
 
@@ -80,13 +83,16 @@ class ObjectCache(object):
 
 def get_space() -> List['Dimension']:
     space = [
-        Integer(6, 40, name='max_depth'),
+        Integer(6, 60, name='max_depth'),
         Integer(20, 100, name='num_leaves'),
         Integer(40, 100, name='n_estimators'),
-        Integer(1, 20, name='n_smooth'),
+        Integer(1, 10, name='n_smooth'),
         Integer(2, 40, name='n_target'),
-        Integer(20, 50, name='num_feats'),
-        Real(0.2, 0.9, name='filter_fit_quantile', prior='uniform')
+        Integer(2, 40, name='weights_n_periods'),
+        Integer(20, 80, name='num_feats'),
+        # Integer(12, 12 * 4, name='max_train_size'),
+        Real(0.2, 0.9, name='filter_fit_quantile', prior='uniform'),
+        Real(-0.002, -0.0001, name='order_offset', prior='uniform')
     ]  # type: List[Dimension]
 
     return space
@@ -95,12 +101,18 @@ def get_space() -> List['Dimension']:
 def objective(
         df: pd.DataFrame,
         mfm: MlflowManager,
-        wm: WeightsManager,
+        obc: ObjectCache,
+        wm: WeightsManager = None,
         df_pred: pd.DataFrame = None,
         sm: SignalManager = None,
-        **kw) -> float:
+        n_jobs: int = -1,
+        **kw) -> Union[float, Tuple[float, float]]:
 
-    log.info(f'objective_kw: {kw}')
+    # time penalty
+    start = time.time()
+
+    str_kw = ', '.join([f'{k}={v}' for k, v in kw.items()])  # cleaner log message
+    log.info(str_kw)
     cfg = md.model_cfg('lgbm')
 
     n_smooth = kw.get('n_smooth', cfg['n_smooth_proba'])
@@ -116,57 +128,84 @@ def objective(
         batch_size=24 * 4 * 8,
         filter_fit_quantile=kw.get('filter_fit_quantile', 0.6),
         retrain_feats=False,
-        split_date=cf.D_SPLIT)  # type: Dict[str, Any]
+        split_date=cf.D_SPLIT,
+        max_train_size=kw.get('max_train_size', 48))  # type: Dict[str, Any]
 
+    # create target signal
     if 'n_target' in kw and not sm is None:
-        # TODO cache this series somehow
-        target_signal = Target(n_periods=kw['n_target'])
+        params = dict(n_periods=kw['n_target'])
+        target_signal = obc.get_or_save(Target, params=params, cache=True)
         df = sm.add_signals(df=df, signals=target_signal, force_overwrite=True)
 
+    # filter df_all signals to select features
     if 'num_feats' in kw and not sm is None:
         df = sm.filter_n_feats(df=df, n=kw['num_feats'])
 
+    # create WeightsManager
+    if wm is None and 'weights_n_periods' in kw:
+        params = dict(n_periods=kw['weights_n_periods'], weight_linear=True)
+        wm = obc.get_or_save(WeightsManager, params=params, df=df).register(mfm)
+
+    # add model predictions iteratively
     if df_pred is None:
         df_pred = sk.add_predict_iter(
-            df=df, wm=wm, model=model, **iter_kw)
+            df=df.iloc[:-target_signal.n_periods],
+            wm=wm,
+            model=model,
+            n_jobs=n_jobs,
+            **iter_kw)
 
-    with mlflow.start_run(experiment_id='3'):
+    with mlflow.start_run(experiment_id=MLFLOW_EXP):
         df_pred = df_pred \
             .pipe(md.add_proba_trade_signal, n_smooth=n_smooth)
 
-        strat = make_strat(symbol=cf.SYMBOL, order_offset=-0.0006).register(mfm)
+        strat = make_strat(
+            symbol=cf.SYMBOL,
+            order_offset=kw.get('order_offset', -0.0006)).register(mfm)
 
-        bm = BacktestManager(
-            startdate=cf.D_SPLIT,
-            strat=strat,
-            df=df_pred) \
-            .run(prnt=True, plot_balance=False) \
-            .register(mfm)
+        try:
+            bm = BacktestManager(
+                startdate=cf.D_SPLIT,
+                strat=strat,
+                df=df_pred) \
+                .run(prnt=True, plot_balance=False) \
+                .register(mfm)
 
-        scores = dict(
-            acc=sk.accuracy_score(df_pred.target, df_pred.y_pred),
-            w_acc=sk.weighted_score(df_pred.target, df_pred.y_pred, wm.weights))
+            scores = dict(
+                acc=sk.accuracy_score(df_pred.target, df_pred.y_pred),
+                w_acc=sk.weighted_score(df_pred.target, df_pred.y_pred, wm.weights))
 
-        metrics = scores | dict(
-            batch_size=iter_kw['batch_size'],
-            filter_fit_quantile=iter_kw['filter_fit_quantile'],
-            n_periods_smooth=n_smooth,
-            interval=cf.INTERVAL)
+            iter_keys = ['batch_size', 'filter_fit_quantile', 'max_train_size']
+            metrics = scores | dict(
+                n_periods_smooth=n_smooth,
+                interval=cf.INTERVAL) \
+                | {k: iter_kw[k] for k in iter_keys}
 
-        params = dict(
-            is_iter=True,
-            retrain_feats=iter_kw['retrain_feats'])
+            params = dict(
+                is_iter=True,
+                retrain_feats=iter_kw['retrain_feats'])
 
-        mlflow.log_metrics(metrics)
-        mlflow.log_params(params)
+            mlflow.log_metrics(metrics)
+            mlflow.log_params(params)
+        except Exception as e:
+            log.error('filed backtest')
+            print(e)
+
         mfm.log_all(flush=True)
 
-    return -1 * bm.df_result['ci_monthly'].iloc[0]
+    # return -1 * bm.df_result['ci_monthly'].iloc[0]
+    # return -1 * strat.wallet.sharpe(weighted=True) #, time.time() - start
+    return -1 * strat.wallet.ci_monthly(weighted=True)
 
 
-def run_opt(n_calls: int = 10, df: pd.DataFrame = None, mfm: MlflowManager = None) -> 'OptimizeResult':
+def run_opt(
+        df: pd.DataFrame,
+        n_calls: int = 10,
+        mfm: MlflowManager = None,
+        n_jobs: int = -1) -> 'OptimizeResult':
 
     p_check = p_skopt / 'checkpoint.pkl'
+    obc = ObjectCache()  # save Target and WeightsManager to avoid recalc
 
     if mfm is None:
         mfm = MlflowManager()
@@ -174,20 +213,18 @@ def run_opt(n_calls: int = 10, df: pd.DataFrame = None, mfm: MlflowManager = Non
     # SignalManager
     sm = SignalManager.default().register(mfm)
 
-    if df is None:
-        target_signal = Target(n_periods=10)
-        signals = md.DEFAULT_SIGNALS + [target_signal]  # type: ignore
-        df = sm.add_signals(
-            df=data.default_df(), signals=signals, use_important=True)
-
-    # WeightsManager
-    wm = WeightsManager.from_config(df).register(mfm)
+    # NOTE dont use this, use df_all, cols will be filtered by num_feats
+    # if df is None:
+    #     target_signal = Target(n_periods=10)
+    #     signals = md.DEFAULT_SIGNALS + [target_signal]  # type: ignore
+    #     df = sm.add_signals(
+    #         df=data.default_df(), signals=signals, use_important=True)
 
     space = get_space()
 
     @skopt.utils.use_named_args(space)
     def _objective(**kw):
-        return objective(df=df, mfm=mfm, wm=wm, sm=sm, **kw)
+        return objective(df=df, mfm=mfm, obc=obc, wm=None, sm=sm, n_jobs=n_jobs, **kw)
 
     checkpoint = CheckpointSaver(p_check, store_objective=False)
 
@@ -203,15 +240,14 @@ def run_opt(n_calls: int = 10, df: pd.DataFrame = None, mfm: MlflowManager = Non
         dimensions=space,
         # base_estimator='ET',
         callback=[checkpoint],
-        # acq_func='LCB',
+        # acq_func='EIps',
         acq_func='gp_hedge',
         # acq_func='PI',
-        # xi=0.01,
-        kappa=3,
+        # xi=0.05,
+        kappa=3.5,
         verbose=True,
         n_calls=n_calls,
-        # random_state=0,
-        n_initial_points=30,
+        n_initial_points=min(n_calls, 30),
         x0=x0,
         y0=y0)
 
@@ -238,8 +274,10 @@ def plot_conv():
 def get_best_results(
         df: pd.DataFrame = None,
         top_n: int = 50,
-        exp_id: str = '3') -> Dict[str, dict[str, float]]:
+        exp_id: str = MLFLOW_EXP,
+        by: str = 'ci_monthly') -> Dict[str, dict[str, float]]:
     """Get dict of median of best params to save to static config file
+    - TODO find better way to avg best results. weighted mean?
 
     Parameters
     ----------
@@ -265,15 +303,18 @@ def get_best_results(
         df = mfm.df_results(experiment_ids=exp_id)
 
     # TODO put this in config somewhere? idk
-    cols = ['max_depth', 'n_estimators', 'num_leaves', 'num_feats',
-            'n_periods_smooth', 'target_n_periods', 'filter_fit_quantile']
+    cols = [
+        'max_depth', 'n_estimators', 'num_leaves',
+        'num_feats', 'n_periods_smooth',
+        'target_n_periods', 'filter_fit_quantile',
+        'weights_n_periods', 'order_offset']
 
     int_cols = df.select_dtypes(int).columns.tolist()
 
     return df \
         .reset_index() \
         .drop_duplicates('ci_monthly') \
-        .sort_values('ci_monthly', ascending=False) \
+        .sort_values(by=by, ascending=False) \
         .head(top_n) \
         .groupby('symbol')[cols] \
         .median() \
