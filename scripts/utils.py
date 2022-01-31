77b738d66390c50f7dd3a1aef7514a7518662e24
Jayme Gordon <jaymegordo@gmail.com>
2022-01-31 13:30:24 -0800

    Add symbol/exch_name for skopt

--- scripts/utils.py
+++ scripts/utils.py
@@ -6,6 +6,7 @@ from jambot.livetrading import ExchangeManager
 from jambot.ml.models import DEFAULT_SIGNALS
 from jambot.ml.storage import ModelStorageManager
 from jambot.signals import SignalManager
+from jambot.tradesys.symbols import Symbols
 from jambot.utils import skopt as sko
 from jambot.utils.mlflow import MlflowManager
 
@@ -32,15 +33,25 @@ cli.add_argument(
     '--skopt',
     type=int,
     default=200,
-    # action='store_true',
     help='Run skopt optimization')
 
 cli.add_argument(
     '--n_jobs',
     type=int,
     default=-1,
-    # action='store_true',
     help='n_jobs for skopt')
+
+cli.add_argument(
+    '--symbol',
+    type=str,
+    default='XBTUSD',
+    help='symbol to optimize')
+
+cli.add_argument(
+    '--exch_name',
+    type=str,
+    default='bitmex',
+    help='exchange to use for symbol data')
 
 
 if __name__ == '__main__':
@@ -52,7 +63,8 @@ if __name__ == '__main__':
 
         # fit models and upload
         em = ExchangeManager()
-        ModelStorageManager().fit_save_models(em=em, overwrite_all=True)
+        # TODO run for multiple symbols
+        ModelStorageManager().fit_save_models(em=em, overwrite_all=True, symbol=a.symbol)
 
         # also upload current "least_important_cols"
         p = cf.p_data / 'feats'
@@ -60,10 +72,13 @@ if __name__ == '__main__':
             BlobStorage(container=p).upload_file(p=p / f'{prefix}_imp_cols.pkl')
 
     elif a.skopt:
+        syms = Symbols()
+        symbol = syms.symbol(a.symbol, exch_name=a.exch_name)
         df_all = SignalManager.default() \
-            .add_signals(df=data.default_df(), signals=DEFAULT_SIGNALS)
+            .add_signals(df=data.default_df(symbol=symbol), signals=DEFAULT_SIGNALS)
 
         log.info(f'df_all: {df_all.shape}')
 
         mfm = MlflowManager()
-        res = sko.run_opt(n_calls=a.skopt, df=df_all, mfm=mfm, n_jobs=a.n_jobs)
+        res = sko.run_opt(
+            n_calls=a.skopt, df=df_all, mfm=mfm, n_jobs=a.n_jobs, symbol=symbol)
