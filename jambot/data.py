2bff299d1d30cb6c8cadb01417cfdf853e461e40
Jayme Gordon <jaymegordo@gmail.com>
2022-01-31 13:29:28 -0800

    Load dfs for multiple symbols

--- jambot/data.py
+++ jambot/data.py
@@ -4,11 +4,16 @@ Loading dataframe
 
 import pandas as pd
 
+from jambot import SYMBOL
 from jambot import config as cf
+from jambot import getlog
+
+log = getlog(__name__)
 
 
-def default_df() -> pd.DataFrame:
+def default_df(symbol: str = SYMBOL) -> pd.DataFrame:
     """Get simple default df for testing"""
-    p = cf.p_data / 'feather/df.ftr'
+    log.info(f'Loading df for "{symbol}" from file')
+    p = cf.p_data / f'feather/df_{symbol.lower()}.ftr'
     return pd.read_feather(p) \
         .set_index('timestamp')
