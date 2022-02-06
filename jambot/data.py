49aa4ac3ee18a4b2649b9d1e36ed70455cd14847
Jayme Gordon <jaymegordo@gmail.com>
2022-02-06 10:23:20 -0800

    Default data based on symbol/exchange

--- jambot/data.py
+++ jambot/data.py
@@ -11,9 +11,9 @@ from jambot import getlog
 log = getlog(__name__)
 
 
-def default_df(symbol: str = SYMBOL) -> pd.DataFrame:
+def default_df(symbol: str = SYMBOL, exch_name: str = 'bitmex') -> pd.DataFrame:
     """Get simple default df for testing"""
-    log.info(f'Loading df for "{symbol}" from file')
-    p = cf.p_data / f'feather/df_{symbol.lower()}.ftr'
+    log.info(f'Loading df {symbol}, {exch_name} from file')
+    p = cf.p_data / f'feather/df_{exch_name.lower()}_{symbol.lower()}.ftr'
     return pd.read_feather(p) \
         .set_index('timestamp')
