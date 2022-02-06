fd26c723010479647fa44b7d2aa763ac1168823a
Jayme Gordon <jaymegordo@gmail.com>
2022-02-06 10:33:55 -0800

    Symbol for default_df testing

--- tests/tradesys/__init__.py
+++ tests/tradesys/__init__.py
@@ -1,11 +1,24 @@
+import pandas as pd
+
 from jambot import data
 from jambot.tradesys.base import Clock
+from jambot.tradesys.symbols import Symbol
 
 from ..__init__ import *
 
 
+@fixture(scope='session')
+def default_df(symbol: Symbol, exch_name: str) -> pd.DataFrame:
+    """Get default OHLC data for single symbol"""
+
+    # NOTE temp kinda ugly
+    if symbol == 'BTCUSD':
+        symbol = 'XBTUSD'
+
+    return data.default_df(symbol=symbol, exch_name=exch_name)
+
+
 @fixture
-def clock():
-    df = data.default_df()
-    clock = Clock(df=df)
-    return clock
+def clock(default_df: pd.DataFrame) -> Clock:
+    """Get Clock timer obj"""
+    return Clock(df=default_df)
