e265666d497e9533348646f63b57ca5bb9f5b9fb
Jayme Gordon <jaymegordo@gmail.com>
2022-02-21 20:11:05 -0800

    Add signals grouped by symbol

--- jambot/ml/models.py
+++ jambot/ml/models.py
@@ -2,11 +2,6 @@ import copy
 from typing import *
 
 import pandas as pd
-from sklearn.base import BaseEstimator
-from sklearn.decomposition import PCA  # noqa
-from sklearn.linear_model import Ridge
-from sklearn.multioutput import MultiOutputRegressor
-from sklearn.pipeline import Pipeline
 
 from jambot import SYMBOL
 from jambot import config as cf
@@ -14,8 +9,17 @@ from jambot import getlog
 from jambot import signals as sg
 from jambot import sklearn_utils as sk
 from jambot.ml.classifiers import LGBMClsLog
+from jambot.signals import SignalManager
 from jgutils import fileops as jfl
 from jgutils import pandas_utils as pu
+
+# from sklearn.linear_model import Ridge
+# from sklearn.multioutput import MultiOutputRegressor
+
+
+if TYPE_CHECKING:
+    from sklearn.base import BaseEstimator
+    from sklearn.pipeline import Pipeline
 
 log = getlog(__name__)
 
@@ -57,14 +61,14 @@ def model_cfg(name: str) -> Dict[str, Any]:
             model_cls=LGBMClsLog,
             n_smooth_proba=3,
         ),
-        ridge=dict(
-            target=['target_max', 'target_min'],
-            target_kw=dict(n_periods=4),
-            target_cls=sg.TargetMaxMin,
-            drop_cols=['target'],
-            model_kw=dict(estimator=Ridge(random_state=0)),
-            model_cls=MultiOutputRegressor,
-        )
+        # ridge=dict(
+        #     target=['target_max', 'target_min'],
+        #     target_kw=dict(n_periods=4),
+        #     target_cls=sg.TargetMaxMin,
+        #     drop_cols=['target'],
+        #     model_kw=dict(estimator=Ridge(random_state=0)),
+        #     model_cls=MultiOutputRegressor,
+        # )
     ).get(name, {})
 
 
@@ -74,9 +78,9 @@ def add_signals(
         symbol: str = SYMBOL,
         drop_ohlc: bool = False,
         use_important_dynamic: bool = True,
-        # use_important: bool = False,
-) -> pd.DataFrame:
-    """Add signal cols to df
+        **kw) -> pd.DataFrame:
+    """Add signal cols + target to df
+    - group by symbol
 
     Parameters
     ----------
@@ -85,6 +89,7 @@ def add_signals(
     name : str
         model name
     symbol : str
+        if MULTI_ALTS will add default signals with same params for all alts
     drop_ohlc : bool
         drop ohlcv columns from raw df, default False
     use_important_dynamic : bool
@@ -102,13 +107,24 @@ def add_signals(
 
     signals = DEFAULT_SIGNALS + [target_signal]
 
-    return sg.SignalManager.default() \
-        .add_signals(
+    return df.groupby('symbol', group_keys=False) \
+        .apply(lambda df: SignalManager.default().add_signals(
             df=df,
             signals=signals,
-            # use_important=use_important,
             use_important_dynamic=use_important_dynamic,
-            drop_ohlc=drop_ohlc)
+            drop_ohlc=drop_ohlc,
+            symbol=symbol,
+            **kw))
+
+    # return SignalManager.default() \
+    #     .add_signals(
+    #         df=df,
+    #         signals=signals,
+    #         # use_important=use_important,
+    #         use_important_dynamic=use_important_dynamic,
+    #         symbol=symbol,
+    #         drop_ohlc=drop_ohlc,
+    #         **kw)
 
 
 def make_model_manager(
@@ -179,7 +195,7 @@ def make_model(name: str, symbol: str = SYMBOL) -> LGBMClsLog:
     return model.from_config(symbol=symbol)
 
 
-def make_pipeline(name: str, df: pd.DataFrame) -> Pipeline:
+def make_pipeline(name: str, df: pd.DataFrame) -> 'Pipeline':
     """Create pipeline to fit/predict on data
 
     Parameters
@@ -200,7 +216,7 @@ def make_pipeline(name: str, df: pd.DataFrame) -> Pipeline:
     return mm.make_pipe(name=name, model=model, steps=None)
 
 
-def add_preds_probas(df: pd.DataFrame, pipe: BaseEstimator, **kw) -> pd.DataFrame:
+def add_preds_probas(df: pd.DataFrame, pipe: 'BaseEstimator', **kw) -> pd.DataFrame:
     """Convenience func to add y_pred, predict_probas, and trade signal cols to df
     - specific to LGBM model with smoothed rolling probas
 
